bebb4f54e4b37b82772c0d52f5c7dba726b624cd
Tanvir-Alam625 <mdtanviralamht731@gmail.com>
2023-05-28 14:42:59 +0600

    init: server initialize

---
+++ server.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
+// const DBConnect = require("./utils/dbConnect");
+
+const app = require("./app");
+
+// database connection
+// DBConnect();
+const port = process.env.PORT || 8080;
+// ESM
+
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`.yellow.bold);
+});
