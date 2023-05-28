46b8f98281c6ded2a748be60ac22ec516e619a2d
Tanvir-Alam625 <mdtanviralamht731@gmail.com>
2023-05-29 00:30:38 +0600

    chore: add limiter and errorhandler middleware

--- routes/v1/product.route.js
+++ routes/v1/product.route.js
@@ -4,10 +4,18 @@ const {
   createProduct,
   getSingleProduct,
 } = require("../../controllers/product.controller");
+const limiter = require("../../middlewares/limiters");
+const errorHandler = require("../../middlewares/errorhandlers");
 
 const router = express.Router();
+const allMiddleware = { limiter, errorHandler };
 
-router.route("/").get(getProducts).post(createProduct);
-router.route("/:id").get(getSingleProduct);
+router
+  .route("/")
+  .get(limiter, errorHandler, getProducts)
+  .post(limiter, errorHandler, createProduct);
+
+// Each Single Products
+router.route("/:id").get(limiter, errorHandler, getSingleProduct);
 
 module.exports = router;
